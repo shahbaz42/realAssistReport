79e1f13dfb24a370f2855d765b06d54795cc4c94
unknown <shahbaz_ali@outlook.in>
2023-10-15 06:48:43 +0530

    add generic pool

--- pdf_generator_server/dist/router/dataRouter.js
+++ pdf_generator_server/dist/router/dataRouter.js
@@ -1,4 +1,5 @@
 "use strict";
+// @ts-nocheck
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,6 +12,63 @@ const puppeteer_1 = __importDefault(require("puppeteer"));
 const path_1 = __importDefault(require("path"));
 const axios_1 = __importDefault(require("axios"));
 const config_1 = require("../config");
+const generic_pool_1 = __importDefault(require("generic-pool"));
+const POOL_MAX = 2;
+const POOL_MIN = 1;
+const PAGE_MAX = 2;
+const minimal_args = [
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain',
+];
+const factory = {
+    create: async () => {
+        const browser = await puppeteer_1.default.launch({
+            headless: "new",
+        }, minimal_args);
+        return browser;
+    },
+    destroy: async (browser) => {
+        await browser.close();
+    }
+};
+const browserPool = generic_pool_1.default.createPool(factory, {
+    max: POOL_MAX,
+    min: POOL_MIN,
+});
+let count = 0;
 const router = express_1.default.Router();
 router.get('/', (req, res) => {
     const dataIndexArray = [
@@ -39,54 +97,20 @@ router.get('/', (req, res) => {
 router.get('/report', async (req, res) => {
     const crimeData = await axios_1.default.get(config_1.CRIME_API_URL);
     const data = crimeData.data.data;
-    const data2 = [{
-            data_year: 2019,
-            'Aggravated Assault': 2077,
-            'All Other Offenses (Except Traffic)': 10179,
-            Arson: 49,
-            Burglary: 603,
-            'Curfew and Loitering Law Violations': 0,
-            'Disorderly Conduct': 742,
-            'Driving Under the Influence': 2856,
-            'Drug Abuse Violations - Grand Total': 930,
-            Drunkenness: 31,
-            Embezzlement: 43,
-            'Forgery and Counterfeiting': 78,
-            Fraud: 121,
-            'Gambling - Total': 0,
-            'Human Trafficking - Commercial Sex Acts': 0,
-            'Human Trafficking - Involuntary Servitude': 0,
-            'Larceny - Theft': 1885,
-            'Liquor Laws': 310,
-            'Manslaughter by Negligence': 4,
-            'Motor Vehicle Theft': 476,
-            'Murder and Nonnegligent Manslaughter': 46,
-            'Offenses Against the Family and Children': 150,
-            'Prostitution and Commercialized Vice': 5,
-            Rape: 129,
-            Robbery: 310,
-            'Simple Assault': 4862,
-            'Stolen Property: Buying, Receiving, Possessing': 91,
-            Suspicion: 0,
-            Vagrancy: 2,
-            Vandalism: 1161,
-            'Weapons: Carrying, Possessing, Etc.': 259,
-            'Sex Offenses (Except Rape, and Prostitution and Commercialized Vice)': 243
-        }];
-    const years = data.map(point => point.data_year);
-    const burglary_data = data.map(point => point.Burglary);
-    const browser = await puppeteer_1.default.launch({
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-        ],
-    });
+    const years = data.map((point) => point.data_year);
+    const burglary_data = data.map((point) => point.Burglary);
+    // console.log(browserPool.pending);
+    if (browserPool.pending >= POOL_MAX) {
+        // console.log("Service Unavailable");
+        return res.status(503).send("Service Unavailable");
+    }
+    let browser = null;
     try {
-        const html = await ejs_1.default.renderFile(path_1.default.resolve(".", "src", "views", "report.ejs"), {
+        browser = await browserPool.acquire();
+        const html = await ejs_1.default.renderFile(path_1.default.resolve('.', 'src', 'views', 'report.ejs'), {
             years,
             burglary_data,
-            DEPLOYED_URI: config_1.DEPLOYED_URI
+            DEPLOYED_URI: config_1.DEPLOYED_URI,
         });
         const result = await pdf_report_with_header_footer_1.default.pdfFromHTML(browser, html, {
             format: 'a4',
@@ -103,18 +127,21 @@ router.get('/report', async (req, res) => {
         res.send(pdfBuffer);
     }
     finally {
-        await browser.close();
+        // console.log("Closing Page");
+        if (browser !== null) {
+            await browserPool.release(browser);
+        }
     }
 });
 router.get('/previewReport', async (req, res) => {
     const crimeData = await axios_1.default.get(config_1.CRIME_API_URL);
     const data = crimeData.data.data;
-    const years = data.map(point => point.data_year);
-    const burglary_data = data.map(point => point.Burglary);
-    res.render(path_1.default.resolve(".", "src", "views", "report.ejs"), {
+    const years = data.map((point) => point.data_year);
+    const burglary_data = data.map((point) => point.Burglary);
+    res.render(path_1.default.resolve('.', 'src', 'views', 'report.ejs'), {
         years,
         burglary_data,
-        DEPLOYED_URI: config_1.DEPLOYED_URI
+        DEPLOYED_URI: config_1.DEPLOYED_URI,
     });
 });
 exports.default = router;
--- pdf_generator_server/src/router/dataRouter.ts
+++ pdf_generator_server/src/router/dataRouter.ts
@@ -1,12 +1,76 @@
+// @ts-nocheck
+
 import express from 'express';
 import ejs from 'ejs';
 import { generateRandomData } from '../utils';
-import report from "../lib/pdf-report-with-header-footer";
-import puppeteer from "puppeteer";
-import path from "path";
+import report from '../lib/pdf-report-with-header-footer';
+import puppeteer from 'puppeteer';
+import path from 'path';
 import fs from 'fs';
 import axios from 'axios';
 import { CRIME_API_URL, DEPLOYED_URI } from '../config';
+import genericPool from 'generic-pool';
+
+const POOL_MAX = 2;
+const POOL_MIN = 1;
+const PAGE_MAX = 2;
+
+const minimal_args = [
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain',
+];
+
+const factory = {
+    create: async () => {
+        const browser = await puppeteer.launch({
+            headless: "new",
+        }, minimal_args);
+        return browser;
+    },
+    destroy: async (browser) => {
+        await browser.close();
+    }
+};
+
+const browserPool = genericPool.createPool(factory, {
+    max: POOL_MAX,
+    min: POOL_MIN,
+});
+
+let count = 0;
 
 const router = express.Router();
 
@@ -73,61 +137,31 @@ interface CrimeData {
 }
 
 router.get('/report', async (req, res) => {
-
     const crimeData = await axios.get(CRIME_API_URL);
     const data = crimeData.data.data as CrimeData[];
-    const data2 = [{
-        data_year: 2019,
-        'Aggravated Assault': 2077,
-        'All Other Offenses (Except Traffic)': 10179,
-        Arson: 49,
-        Burglary: 603,
-        'Curfew and Loitering Law Violations': 0,
-        'Disorderly Conduct': 742,
-        'Driving Under the Influence': 2856,
-        'Drug Abuse Violations - Grand Total': 930,
-        Drunkenness: 31,
-        Embezzlement: 43,
-        'Forgery and Counterfeiting': 78,
-        Fraud: 121,
-        'Gambling - Total': 0,
-        'Human Trafficking - Commercial Sex Acts': 0,
-        'Human Trafficking - Involuntary Servitude': 0,
-        'Larceny - Theft': 1885,
-        'Liquor Laws': 310,
-        'Manslaughter by Negligence': 4,
-        'Motor Vehicle Theft': 476,
-        'Murder and Nonnegligent Manslaughter': 46,
-        'Offenses Against the Family and Children': 150,
-        'Prostitution and Commercialized Vice': 5,
-        Rape: 129,
-        Robbery: 310,
-        'Simple Assault': 4862,
-        'Stolen Property: Buying, Receiving, Possessing': 91,
-        Suspicion: 0,
-        Vagrancy: 2,
-        Vandalism: 1161,
-        'Weapons: Carrying, Possessing, Etc.': 259,
-        'Sex Offenses (Except Rape, and Prostitution and Commercialized Vice)': 243
-    }]
-    
-    const years = data.map(point => point.data_year)
-    const burglary_data = data.map(point => point.Burglary); 
 
-    const browser = await puppeteer.launch({
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-        ],
-    });
+    const years = data.map((point) => point.data_year);
+    const burglary_data = data.map((point) => point.Burglary);
+
+    // console.log(browserPool.pending);
+    if(browserPool.pending >= POOL_MAX) {
+        // console.log("Service Unavailable");
+        return res.status(503).send("Service Unavailable");
+    }
+    let browser = null;
+
 
     try {
-        const html = await ejs.renderFile(path.resolve(".", "src", "views", "report.ejs"),{
-            years,
-            burglary_data,
-            DEPLOYED_URI
-        })
+        browser = await browserPool.acquire();
+
+        const html = await ejs.renderFile(
+            path.resolve('.', 'src', 'views', 'report.ejs'),
+            {
+                years,
+                burglary_data,
+                DEPLOYED_URI,
+            }
+        );
 
         const result = await report.pdfFromHTML(browser, html, {
             format: 'a4',
@@ -138,27 +172,30 @@ router.get('/report', async (req, res) => {
                 top: '4mm',
             },
         });
-        
+
         const pdfBuffer = Buffer.from(result);
 
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'inline; filename=report.pdf');
         res.send(pdfBuffer);
     } finally {
-        await browser.close();
+        // console.log("Closing Page");
+        if(browser !== null) {
+            await browserPool.release(browser);
+        }
     }
 });
 
 router.get('/previewReport', async (req, res) => {
     const crimeData = await axios.get(CRIME_API_URL);
     const data = crimeData.data.data as CrimeData[];
-    
-    const years = data.map(point => point.data_year)
-    const burglary_data = data.map(point => point.Burglary); 
-    res.render(path.resolve(".", "src", "views", "report.ejs") , {
+
+    const years = data.map((point) => point.data_year);
+    const burglary_data = data.map((point) => point.Burglary);
+    res.render(path.resolve('.', 'src', 'views', 'report.ejs'), {
         years,
         burglary_data,
-        DEPLOYED_URI
+        DEPLOYED_URI,
     });
 });
 
